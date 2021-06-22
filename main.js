929446876e297c1c52be275f1158439f573731b7
qmsiteandy <ht93084@gmail.com>
2021-06-22 14:16:04 +0800

    update

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// client 與 server 建立連線
 const socket = io.connect("https://socket-chat-server-test-202106.herokuapp.com/", {transports: ["websocket"],});
 
 $(document).ready(() => {
@@ -9,6 +10,7 @@ $(document).ready(() => {
     form.addEventListener('submit', function(e) {
         e.preventDefault();
         if (name.value && msg.value) {
+            // emit指令：傳送資料到server
             socket.emit('sendMessage', {
                 name: name.value,
                 msg: msg.value
@@ -17,12 +19,14 @@ $(document).ready(() => {
         }
     });
 
+    // on指令：當接收到server傳來的資料
     socket.on("newMessage", function (data) {
         $("#messages").append(`
             <li>
                 ${data.name}：${data.msg}
             </li>
         `);
+        
     window.scrollTo(0, document.body.scrollHeight);
   });
 });
