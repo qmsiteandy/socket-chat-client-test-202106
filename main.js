a1990fe6da641cd0d432c3c1c170c282023a3ec6
qmsiteandy <ht93084@gmail.com>
2021-06-22 13:33:17 +0800

    init

---
+++ main.js
@@ -0,0 +1,28 @@
+const socket = io.connect("https://socket-chat-server-test-202106.herokuapp.com/", {transports: ["websocket"],});
+
+$(document).ready(() => {
+
+    var form = document.getElementById('form');
+    var name = document.getElementById('name');
+    var msg = document.getElementById('msg');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        if (name.value && msg.value) {
+            socket.emit('sendMessage', {
+                name: name.value,
+                msg: msg.value
+            });
+            msg.value = '';
+        }
+    });
+
+    socket.on("sendMessage", function (data) {
+        $("#messages").append(`
+            <li>
+                ${data.name}：${data.msg}
+            </li>
+        `);
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+});
